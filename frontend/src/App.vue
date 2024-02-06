43c9509e2088dd52b9341acc3c90e2b8c8d95ba3
Callet <elliot.callet@hotmail.com>
2024-02-06 15:04:37 +0100

    Revert "retirer logos vue"
    This reverts commit 25e864822d5377b2c57c2273bf1d91e447a822a0.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,10 +3,15 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div><HelloWorld msg="Page de Elliot Callet" />
   <div>
-    <HelloWorld msg="Page de Elliot Callet" />
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
   </div>
+  <HelloWorld msg="Rendu site" />
 </template>
 
 <style scoped>
@@ -23,4 +28,3 @@ import HelloWorld from './components/HelloWorld.vue'
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
-}</style>
